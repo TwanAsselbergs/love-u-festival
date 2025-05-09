3a6ee8fb1eb15464424ceeec1abe77fcec1c4c07
TwanAsselbergs <twanasselbergs@outlook.com>
2025-05-09 22:06:03 +0200

    Install VItePWA plugin and set up manifest in vite.config.js

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -1,7 +1,39 @@
-import tailwindcss from '@tailwindcss/vite';
-import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true
+			},
+			manifest: {
+				name: 'Love U Festival',
+				short_name: 'LoveU',
+				description: 'A PWA for festival-goers',
+				theme_color: '#ffffff',
+				background_color: '#ffffff',
+				display: 'standalone',
+				icons: [
+					{
+						src: '/icons/logo-white.png',
+						sizes: '192x192',
+						type: 'image/png',
+						purpose: 'any'
+					},
+					{
+						src: '/icons/512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any'
+					}
+				]
+			}
+		})
+	]
 });
