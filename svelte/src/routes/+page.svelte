a03a528a5cb22f7fd83720cd11b93988431e83b4
TwanAsselbergs <twanasselbergs@outlook.com>
2025-05-10 21:24:14 +0200

    Add map to landing page

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -1,5 +1,10 @@
-<script></script>
+<script>
+	import LanguageSwitcher from '../components/LanguageSwitcher.svelte';
+	import { t } from 'svelte-i18n';
+</script>
+
+<LanguageSwitcher />
 
 <main class="flex min-h-screen w-full items-center justify-center text-center">
-	<h1>Map</h1>
+	<img src="/img/map.png" alt="Map" class="" />
 </main>
