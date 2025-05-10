d45ec7be0dd8fc55708be3f157aa90c684458374
TwanAsselbergs <twanasselbergs@outlook.com>
2025-05-10 21:25:07 +0200

    Add language switcher dropdown to all pages

--- svelte/src/routes/schedule/+page.svelte
+++ svelte/src/routes/schedule/+page.svelte
@@ -1,5 +1,10 @@
-<script></script>
+<script>
+	import LanguageSwitcher from '../../components/LanguageSwitcher.svelte';
+	import { t } from 'svelte-i18n';
+</script>
+
+<LanguageSwitcher />
 
 <main class="flex min-h-screen w-full items-center justify-center text-center">
-	<h1 class="font-extralight">Schedule</h1>
+	<h1 class="">{$t('schedule')}</h1>
 </main>
