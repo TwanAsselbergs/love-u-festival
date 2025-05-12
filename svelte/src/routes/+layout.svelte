5bef33bf132cbc4537896ce945f00129abda64ad
TwanAsselbergs <twanasselbergs@outlook.com>
2025-05-12 11:37:34 +0200

    Add color theme localStorage to +layout.svelte

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -3,6 +3,15 @@
 	import '$lib/i18ns';
 	import Navbar from '../components/Navbar.svelte';
 	import LanguageSwitcher from '../components/LanguageSwitcher.svelte';
+
+	if (typeof window !== 'undefined') {
+		const theme = localStorage.getItem('theme');
+		if (theme === 'dark') {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	}
 </script>
 
 <Navbar />
