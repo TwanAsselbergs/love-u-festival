2066fa518fd8b1f1c5333b2470e896974440e546
TwanAsselbergs <twanasselbergs@outlook.com>
2025-05-12 09:11:28 +0200

    Add LanguageSwitcher.svelte to global layout

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -2,7 +2,9 @@
 	import '../app.css';
 	import '$lib/i18ns';
 	import Navbar from '../components/Navbar.svelte';
+	import LanguageSwitcher from '../components/LanguageSwitcher.svelte';
 </script>
 
 <Navbar />
+<LanguageSwitcher />
 <slot />
