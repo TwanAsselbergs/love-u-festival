ec79a9996a979f3523def725e81dbaea4cb71f3e
TwanAsselbergs <twanasselbergs@outlook.com>
2025-05-12 09:10:45 +0200

    Remove German language and update JSON language files

--- svelte/src/components/LanguageSwitcher.svelte
+++ svelte/src/components/LanguageSwitcher.svelte
@@ -3,7 +3,7 @@
 	import { locale } from 'svelte-i18n';
 	import { slide } from 'svelte/transition';
 
-	let currentLang = 'en';
+	let currentLang = 'nl';
 	let isOpen = false;
 
 	function switchLang(lang) {
@@ -19,45 +19,32 @@
 			currentLang = savedLang;
 			locale.set(savedLang);
 		} else {
-			locale.set('en');
+			locale.set('nl');
 		}
 	});
 </script>
 
 <div class="absolute right-5 top-5 z-20">
-	<button
-		class="flex items-center gap-2 rounded bg-white/75 p-2 backdrop-blur"
-		on:click={() => (isOpen = !isOpen)}
-	>
+	<button class="flex items-center gap-2 rounded p-2" on:click={() => (isOpen = !isOpen)}>
 		<img src={`/img/flags/${currentLang}.png`} alt={currentLang} class="h-6" />
 	</button>
 </div>
 
 {#if isOpen}
 	<div
-		class="top-13 absolute right-5 mt-2 w-40 rounded-3xl bg-white shadow"
-		transition:slide={{ duration: 300 }}
-	>
+		class="top-13 dark:bg-gray3 absolute right-5 z-20 mt-2 w-40 rounded-3xl bg-white/40 shadow backdrop-blur-lg dark:text-white/90"
+		transition:slide={{ duration: 300 }}>
 		<button
-			class="flex w-full items-center gap-3 px-4 py-4 text-left"
-			on:click={() => switchLang('en')}
-		>
-			<img src="/img/flags/en.png" alt="English" class="h-6" />
-			<span>English</span>
-		</button>
-		<button
-			class="pb- flex w-full items-center gap-3 px-4 pb-4 text-left"
-			on:click={() => switchLang('nl')}
-		>
+			class="pb- flex w-full items-center gap-3 px-4 py-4 text-left"
+			on:click={() => switchLang('nl')}>
 			<img src="/img/flags/nl.png" alt="English" class="h-6" />
 			<span>Nederlands</span>
 		</button>
 		<button
 			class="flex w-full items-center gap-3 px-4 pb-4 text-left"
-			on:click={() => switchLang('de')}
-		>
-			<img src="/img/flags/de.png" alt="English" class="h-6" />
-			<span>Deutsch</span>
+			on:click={() => switchLang('en')}>
+			<img src="/img/flags/en.png" alt="English" class="h-6" />
+			<span>English</span>
 		</button>
 	</div>
 {/if}
--- svelte/src/lib/i18ns.js
+++ svelte/src/lib/i18ns.js
@@ -2,7 +2,6 @@ import { register, init, getLocaleFromNavigator } from 'svelte-i18n';
 
 register('en', () => import('./locales/en.json'));
 register('nl', () => import('./locales/nl.json'));
-register('de', () => import('./locales/de.json'));
 
 init({
 	fallbackLocale: 'en',
