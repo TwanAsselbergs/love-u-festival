fa025c24be7046641b1bd4925c9d275c8905ea70
TwanAsselbergs <twanasselbergs@outlook.com>
2025-05-14 23:35:16 +0200

    Change map page location

---
+++ svelte/src/routes/map/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { t } from 'svelte-i18n';
+</script>
+
+<main class="flex min-h-screen w-full items-center justify-center text-center">
+	<img src="/img/map.png" alt="Map" class="" />
+</main>
