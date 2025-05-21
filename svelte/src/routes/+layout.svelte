5a66d5e259ec4775732710f868644ee1ef20ad83
TwanAsselbergs <twanasselbergs@outlook.com>
2025-05-21 21:15:53 +0200

    Import Service Worker registration to +layout.svelte

--- svelte/src/routes/+layout.svelte
+++ svelte/src/routes/+layout.svelte
@@ -4,13 +4,25 @@
 	import Navbar from '../components/Navbar.svelte';
 	import LanguageSwitcher from '../components/LanguageSwitcher.svelte';
 
-	if (typeof window !== 'undefined') {
+	import { browser } from '$app/environment';
+	import { onMount } from 'svelte';
+	import { registerSW } from 'virtual:pwa-register';
+
+	if (browser) {
 		const theme = localStorage.getItem('theme');
 		if (theme === 'dark') {
 			document.documentElement.classList.add('dark');
 		} else {
 			document.documentElement.classList.remove('dark');
 		}
+
+		onMount(() => {
+			registerSW({
+				onOfflineReady() {
+					console.log('App is ready to work offline');
+				}
+			});
+		});
 	}
 </script>
 
