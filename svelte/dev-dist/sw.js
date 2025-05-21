632fca6c799cb7ce409853abd24eb0ea20b1f44c
TwanAsselbergs <twanasselbergs@outlook.com>
2025-05-21 23:20:43 +0200

    Update Service Worker revision

--- svelte/dev-dist/sw.js
+++ svelte/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.g05tjhf1ga"
+    "revision": "0.7ouerkd5cv8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
