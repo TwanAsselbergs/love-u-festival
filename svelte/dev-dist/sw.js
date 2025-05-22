43b5af7f2174843d73edfea17b649581c78beb5d
TwanAsselbergs <twanasselbergs@outlook.com>
2025-05-22 23:47:58 +0200

    Renew sw.js revision

--- svelte/dev-dist/sw.js
+++ svelte/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.7ouerkd5cv8"
+    "revision": "0.pe98c51hv48"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
