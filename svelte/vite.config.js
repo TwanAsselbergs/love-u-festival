6d12a10531029ad07067cff981431e3f2a8b8bc0
TwanAsselbergs <twanasselbergs@outlook.com>
2025-05-28 10:21:11 +0200

    Add service worker auto registration and manifest icons

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -4,13 +4,15 @@ import { VitePWA } from 'vite-plugin-pwa';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+	base: '/love-u-festival/',
 	plugins: [
 		tailwindcss(),
 		sveltekit(),
 		VitePWA({
+			injectRegister: 'auto',
 			registerType: 'autoUpdate',
 			devOptions: {
-				enabled: true
+				enabled: false
 			},
 			manifest: {
 				name: 'Love U Festival',
@@ -21,23 +23,44 @@ export default defineConfig({
 				display: 'standalone',
 				icons: [
 					{
-						src: '/icons/logo-white.png',
-						sizes: '192x192',
+						src: '/love-u-festival/img/icons/144.png',
+						sizes: '144x144',
 						type: 'image/png',
 						purpose: 'any'
 					},
 					{
-						src: '/icons/512.png',
+						src: '/love-u-festival/img/icons/192.png',
+						sizes: '1024x1024',
+						type: 'image/png',
+						purpose: 'any'
+					},
+					{
+						src: '/love-u-festival/img/icons/512.png',
 						sizes: '512x512',
 						type: 'image/png',
 						purpose: 'any'
 					}
-				],
-				splash_pages: [
+				]
+			},
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+				maximumFileSizeToCacheInBytes: 10 * 1024 * 1024,
+				runtimeCaching: [
 					{
-						src: '/icons/logo-white.png',
-						sizes: '192x192',
-						type: 'image/png'
+						urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'google-fonts-stylesheets',
+							expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 }
+						}
+					},
+					{
+						urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'google-fonts-webfonts',
+							expiration: { maxEntries: 10, maxAgeSeconds: 60 * 60 * 24 * 365 }
+						}
 					}
 				]
 			}
